86ac37f9dd34470b5b0c15eccb235c38d7810114
Mikuo_390 <2992868880@qq.com>
2026-09-04 14:54:58 +0800

    优化角斗场配队算法,修复了领取奖励卡死的问题

--- agent/jdc_select_character.py
+++ agent/jdc_select_character.py
@@ -14,7 +14,7 @@ from maa.context import Context
 JDC_TARGET = {
     "dps": 100,
     "np": 90,
-    "heal": 80,
+    "heal": 70,
     "buff": 60
 }
 
@@ -34,7 +34,7 @@ JDC_PRIORITIES = {
 
 JDC_INCLUDE_DISABLED = False
 
-JDC_NAME_MATCH_RATIO = 0.72
+JDC_NAME_MATCH_RATIO = 0.50
 
 
 # ============================================================
